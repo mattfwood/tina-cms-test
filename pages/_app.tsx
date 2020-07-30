aa91a5e4c9789da1a023fca11601075f5bf76a46
Matt Wood <matt@cratebind.com>
2020-07-30 10:53:09 -0500

    Initial Tina CMS setup

---
+++ pages/_app.tsx
@@ -0,0 +1,88 @@
+import App from 'next/app'
+import { TinaCMS, TinaProvider } from 'tinacms'
+import { GithubClient, TinacmsGithubProvider } from 'react-tinacms-github'
+
+export default class Site extends App {
+  cms: TinaCMS
+
+  constructor(props) {
+    super(props)
+    /**
+     * 1. Create the TinaCMS instance
+     */
+    this.cms = new TinaCMS({
+      enabled: !!props.pageProps.preview,
+      apis: {
+        /**
+         * 2. Register the GithubClient
+         */
+        github: new GithubClient({
+          proxy: '/api/proxy-github',
+          authCallbackRoute: '/api/create-github-access-token',
+          clientId: process.env.GITHUB_CLIENT_ID,
+          baseRepoFullName: process.env.REPO_FULL_NAME, // e.g: tinacms/tinacms.org,
+        }),
+      },
+      /**
+       * 3. Use the Sidebar and Toolbar
+       */
+      sidebar: props.pageProps.preview,
+      toolbar: props.pageProps.preview,
+    })
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      /**
+       * 4. Wrap the page Component with the Tina and Github providers
+       */
+      <TinaProvider cms={this.cms}>
+        <TinacmsGithubProvider
+          onLogin={onLogin}
+          onLogout={onLogout}
+          error={pageProps.error}
+        >
+          {/**
+           * 5. Add a button for entering Preview/Edit Mode
+           */}
+          <EditLink cms={this.cms} />
+          <Component {...pageProps} />
+        </TinacmsGithubProvider>
+      </TinaProvider>
+    )
+  }
+}
+
+const onLogin = async () => {
+  const token = localStorage.getItem('tinacms-github-token') || null
+  const headers = new Headers()
+
+  if (token) {
+    headers.append('Authorization', 'Bearer ' + token)
+  }
+
+  const resp = await fetch(`/api/preview`, { headers: headers })
+  const data = await resp.json()
+
+  if (resp.status == 200) window.location.href = window.location.pathname
+  else throw new Error(data.message)
+}
+
+const onLogout = () => {
+  return fetch(`/api/reset-preview`).then(() => {
+    window.location.reload()
+  })
+}
+
+export interface EditLinkProps {
+  cms: TinaCMS
+}
+
+export const EditLink = ({ cms }: EditLinkProps) => {
+  return (
+    <button onClick={() => cms.toggle()}>
+      {cms.enabled ? 'Exit Edit Mode' : 'Edit This Site'}
+    </button>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,59 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
+import { useGithubJsonForm } from 'react-tinacms-github'
+import { usePlugin } from 'tinacms'
+import { GetStaticProps } from 'next'
+
+export default function Home({ file }) {
+  console.log({ file })
+  const formOptions = {
+    label: 'Home Page',
+    fields: [{ name: 'title', component: 'text' }],
+  }
+
+  const [data, form] = useGithubJsonForm(file, formOptions)
+  usePlugin(form)
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          {data.title}
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+/**
+ * Fetch data with getStaticProps based on 'preview' mode
+ */
+export const getStaticProps: GetStaticProps = async function({
+ preview,
+ previewData,
+}) {
+ if (preview) {
+   return getGithubPreviewProps({
+     ...previewData,
+     fileRelativePath: 'content/home.json',
+     parse: parseJson,
+   })
+ }
+ return {
+   props: {
+     sourceProvider: null,
+     error: null,
+     preview: false,
+     file: {
+       fileRelativePath: 'content/home.json',
+       data: (await import('../content/home.json')).default,
+     },
+   },
+ }
+}
